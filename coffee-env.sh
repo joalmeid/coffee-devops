09145366f7f4a3607c29d883cf65dab3f794230e
João Almeida <joalmeid@microsoft.com>
2018-10-02 12:02:17 +0200

    coffee env

---
+++ coffee-env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export subscriptionId=eec159fb-1c8e-4021-a4a2-1085d22292e2
+export resourceGroupLocation=westeurope
+export teamNumber=gpx5
+export keyVaultName=openhackgpx5kv
+export resourceGroup=openhackgpx5rg
+export ACR=openhackgpx5acr
+export AKS=openhackgpx5aks
+export sqlServerName=openhackgpx5sql
+export sqlServerUserName=openhackgpx5sa
+export sqlServerPassword=8D2wq3Kc9pwd
+export sqlDbName=mydrivingDB
+export teamFiles=/home/azureuser/team_env/openhackgpx5
+export jenkinsVMPassword=dM6292Ig5pwd
+export jenkinsURL=jenkinsopenhackgpx5.westeurope.cloudapp.azure.com:8080
+export SPName=http://azure-cli-2018-09-28-20-36-59
+export SPPass=be788904-08db-4a75-8cb3-0ef085d2c0aa
+export SPID=f0be168b-9e0e-45eb-b03f-455714f01586
+export ACR_URI=/subscriptions/eec159fb-1c8e-4021-a4a2-1085d22292e2/resourceGroups/openhackgpx5rg/providers/Microsoft.ContainerRegistry/registries/openhackgpx5acr
+export ingressIp=23.97.184.50
+export endpoint=akstraefikopenhackgpx5.westeurope.cloudapp.azure.com
+export kubeconfig=/home/azureuser/team_env/openhackgpx5/kubeconfig-openhackgpx5
+export zippassword=--P3mqyfWQ29tNqB%J8eo5wE%y6a99BV
